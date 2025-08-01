535d877876705c7023027164f2259a450ddbc9d4
Arham <arhamali63@gmail.com>
2025-08-01 16:49:48 +0500

    js class 4 code

---
+++ js-class-4/script.js
@@ -0,0 +1,50 @@
+// var student1 = "Sakhawat Abbas"
+// var student2 = "Muhammad Rehan"
+// var student3 = "Muhammad Muneeb"
+// var student3 = "Muhammad Raees"
+
+// var userInput = prompt("Enter your name to push??")
+
+// var students = ["Sakhawat Abbas", "Muhammad Rehan",  "Muhammad Muneeb" , "Muhammad Raees"] //Array
+// // Array index will always starts from 0
+
+// // 0 < 4 = true 
+// for(var i = 0 ; i < students.length ; i++){
+//     console.log(students[i])
+// }
+
+// students.push("Arham")
+// students.pop()
+
+// students.shift()
+// students.unshift("Arham") 
+
+// students.splice(1,0 , "Sufyan" )
+
+
+// console.log(student2)
+// console.log(student1)
+// console.log(student3)
+
+// console.log(students[1]) // "Sakhawat Abbas"
+// console.log(students[10]) // undefined
+// console.log(students)
+
+// students.length = 0
+
+// var copiedStudents = students.slice(2, 4)
+// console.log(copiedStudents)
+// students.push("Muhammad Haris" , "Noman" , "Arham")
+// console.log(students)
+
+// console.log(students[0])
+// console.log(students[1])
+// console.log(students[2])
+// console.log(students[3])
+
+// var userInput = prompt("Enter your table number")
+// var tableLength = prompt("Enter table length")
+
+// for (var i = 1; i <= 10; i++){
+//     console.log( userInput * i)
+// }    
