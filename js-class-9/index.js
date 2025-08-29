628c8f0cf3724dcab83046005bac251ad4b31b35
Saima-Imran90 <smalldeeds1234@gmail.com>
2025-08-29 16:45:49 +0500

    class 9 js code

---
+++ js-class-9/index.js
@@ -0,0 +1,41 @@
+// var dayOfWeek = +prompt("aj kiya din he??")
+
+// switch (dayOfWeek ) {
+//     case "sunday":
+//         alert("han bhai off he aj")
+//         break;
+//     case 19:
+//         alert("han bhai app bh vote de skty ho.")
+//         break;
+//     case 20:
+//         alert("han bhai app bh vote de skty ho.")
+//         break;
+//     default:
+//         alert("ye to din hi nh he")
+// }
+
+// var isRestart = "false"
+
+// while (isRestart == "true"){
+//     console.log("game starting")
+//     console.log("game running")
+//     console.log("game ending")
+
+//     isRestart = prompt("do you want to restart the game?")
+// }
+
+// var isRestart = "false"
+
+// do{
+//     console.log("game starting")
+//     console.log("game running")
+//     console.log("game ending")
+
+//     isRestart = prompt("do you want to restart the game?")
+
+// } while(isRestart === "true")
+
+// function changeImage(src){
+//     var myImage = document.getElementById("img-1")
+//     myImage.src = `https://${src}`
+// }
