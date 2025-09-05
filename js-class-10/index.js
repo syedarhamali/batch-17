4d7f7a0dfd6c272d5c55e9ae5b835a50f6891c43
Saima-Imran90 <smalldeeds1234@gmail.com>
2025-09-05 16:48:46 +0500

    js class 10 code

---
+++ js-class-10/index.js
@@ -0,0 +1,73 @@
+// var userPin = 1234
+// var balance = 10000
+
+// function insertCard(){
+//     var pinInputContainer = document.getElementById("pin-input")
+//     pinInputContainer.style.display = "block"
+// }
+
+// function checkPin(){
+//     console.log("running")
+//     var pin = document.getElementById("pin")
+//     var userOptions = document.getElementById("user-options")
+//     console.log(pin.value , userPin)
+//     if(userPin == pin.value){
+//         alert("Correct Pin!")
+//         userOptions.style.display = "inline-block"
+//     }
+// }
+
+
+// function depositeNotes(){
+//     var depositeAmount = +prompt("Enter your amount to deposite")
+//     balance = balance + depositeAmount
+//     balance += depositeAmount
+//     alert('your new balance is' + " " + balance)
+// }
+
+// function balanceInquiry(){
+//     alert("Your balance is " + " " + balance)
+// }
+
+
+// function expandPara(){
+//     var paragraph = document.getElementById("paragraph")
+//     paragraph.innerText +=  "Hello world 123352352342343"
+// }
+
+// function submitForm(event){
+//     event.preventDefault()
+//     var prevAddress = document.getElementById("personName")
+//     var addressElement = document.getElementById("address")
+//     addressElement.value = prevAddress.value
+// }
+
+
+// let myName = "Arham"
+
+// myName = "Hassan"
+
+// const myName = "" // error
+
+// console.log(myName)
+
+// let myName // global scope
+// console.log(myName)
+// function doSomething(){
+//      myName = "Hello" // local scope
+// }
+// doSomething()
+// console.log(myName)
+
+
+
+
+function doSomething(){
+    if(true){
+        var myName = "Arham"
+        
+    }
+}
+console.log(myName)
+
+doSomething()
