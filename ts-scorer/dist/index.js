8b2b7796c00cd5e225fd0ef48e14f4ba80dc128b
Arham <arhamali63@gmail.com>
2026-01-02 16:41:21 +0500

    class in ts

--- ts-scorer/dist/index.js
+++ ts-scorer/dist/index.js
@@ -1,6 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var personName = 'Arham';
-personName = 'usman';
-console.log(personName);
+import * as readline from 'readline-sync';
+readline.question("Welcome to my application");
+let userName = readline.question("What's your name ??");
+let percentage = readline.question("What's your percentage ??");
+let toNumber = Number(percentage);
+if (toNumber > 50) {
+    console.log(userName + 'you are passed!');
+}
+else {
+    console.log(userName + "you are failed! ");
+}
+class Student {
+    personName;
+    constructor(personName) {
+        this.personName = personName;
+    }
+}
+const user = new Student("Arham");
+console.log(user);
 //# sourceMappingURL=index.js.map
--- ts-scorer/src/index.ts
+++ ts-scorer/src/index.ts
@@ -1,16 +1,27 @@
 import * as readline from 'readline-sync'
 
-
 readline.question("Welcome to my application")
 
 
-let userName:string = readline.question("What's your name ??")
-let percentage:string = readline.question("What's your percentage ??")
+let userName: string = readline.question("What's your name ??")
+let percentage: string = readline.question("What's your percentage ??")
 let toNumber = Number(percentage)
 
 
-if(toNumber > 50){
-    console.log(userName + 'you are passed!' )
-} else{
+if (toNumber > 50) {
+    console.log(userName + 'you are passed!')
+} else {
     console.log(userName + "you are failed! ")
 }
+
+
+class Student {
+    public personName
+    constructor(personName: string) {
+        this.personName = personName
+
+    }
+}
+
+const user = new Student("Arham")
+console.log(user)
